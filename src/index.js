5077bd6e4cf07f62a728e8240934297a87d226aa
monarkier <emma.virkki@outlook.com>
2024-02-27 14:07:10 +0200

    week2

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@
 import express from 'express';
 import path from 'path';
 import {fileURLToPath} from 'url';
+import {deleteItem, getItemById, getItems, postItem, putItem} from './items.mjs';
+import {getUserById, getUsers, postUser, postLogin, putUser} from './users.mjs';
 const hostname = '127.0.0.1';
 const port = 3000;
 const app = express();
 
+app.use(express.json());
 // Staattinen sivusto palvelimen juureen (public-kansion sisältö näkyy osoitteessa http://127.0.0.1:3000/sivu.html)
 app.use(express.static('public'));
 const __filename = fileURLToPath(import.meta.url);
@@ -14,33 +17,31 @@ const __dirname = path.dirname(__filename);
 // Tarjoiltava kansio määritellään relatiivisella polulla
 app.use('/sivusto', express.static(path.join(__dirname, '../public')));
 
-// mock data for simple API
-const items = [
-  {id: 1, name: 'Item 1'},
-  {id: 2, name: 'Item 2'},
-  {id: 3, name: 'Item kolme'},
-  {id: 4, name: 'Item neljä'},
-];
 
+// RESOURCE /item endpoints
 // GET http://127.0.0.1:3000/items
-app.get('/items', (req, res) => {
-  res.json(items);
-});
-
+app.get('/items', getItems);
 // GET http://127.0.0.1:3000/items/<ID>
-app.get('/items/:id', (req, res) => {
-  // TODO: palauta vain se objekti, jonka id vastaa pyydettyä
-  console.log('requested item id', req.params.id);
-  let item = 'tämän tilalle oikea objekti';
-  res.json(item);
-});
+app.get('/items/:id', getItemById);
+// POST http://127.0.0.1:3000/items/ (Itemin lisäys)
+app.post('/items', postItem);
+// PUT
+app.put('/items/:id', putItem);
+// DELETE
+app.delete('/items/:id', deleteItem);
 
-// Itemin lisäys
-// POST http://127.0.0.1:3000/items/
-app.post('/items', (req, res) => {
-  // TODO (vapaaehtonen, jatketaan tästä ens kerralla): lisää postattu item items-taulukkoon
-  res.json({message: 'item created'});
-});
+// Users resource
+// list users
+app.get('/users', getUsers);
+// get info of a user
+app.get('/users/:id', getUserById);
+// user registration
+app.post('/users', postUser);
+// user login
+app.post('/users/login', postLogin);
+// update user
+app.put('/users/:id', putUser);
+
 
 // GET http://127.0.0.1:3000
 // ei toimi tällä hetkellä, koska public-server tarjoilee index.html:n ensin
